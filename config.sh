08c724a0bf0dc86644225ab771a5eb9ed90af1fb
tarekismail84 <tismail@varrow.tech>
2022-07-13 03:10:41 +0200

    aloo

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ wget https://api.wordpress.org/secret-key/1.1/salt/ -O salt.txt
 sed -i '51,58d' wp-config.php
 cat salt.txt >> wp-config.php
 cat extra-wp-config.txt >> wp-config.php
-sed -i 's/$1/database_name_here/g' wp-config.php
-sed -i 's/$2/username_here/g' wp-config.php
-sed -i 's/$3/password_here/g' wp-config.php
-sed -i 's/$4/localhost/g' wp-config.php
+sed -i "s|database_name_here|$1|g" wp-config.php
+sed -i "s|username_here|$2|g" wp-config.php
+sed -i "s|password_here|$3|g" wp-config.php
+sed -i "s|localhost|$4|g" wp-config.php
