e97fc698eb62727de7019d1691d3e0bcc0798250
tarekismail84 <tismail@varrow.tech>
2022-07-13 03:42:17 +0200

    ya mosahel

--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wget https://api.wordpress.org/secret-key/1.1/salt/ -O salt.txt
 sed -i '51,58d' /var/www/html/wp-config.php
-cat salt.txt >> /var/www/html/wp-config.php
-cat extra-wp-config.txt >> /var/www/html/wp-config.php
+cat /var/www/html/salt.txt >> /var/www/html/wp-config.php
+cat /var/www/html/extra-wp-config.txt >> /var/www/html/wp-config.php
 sed -i "s|database_name_here|$1|g" /var/www/html/wp-config.php
 sed -i "s|username_here|$2|g" /var/www/html/wp-config.php
 sed -i "s|password_here|$3|g" /var/www/html/wp-config.php
