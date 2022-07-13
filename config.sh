b227e0d8cd99e99dfb66440522902f3d8c67d49c
tarekismail84 <tismail@varrow.tech>
2022-07-13 19:32:10 +0200

    v8

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-wget https://api.wordpress.org/secret-key/1.1/salt/ -O salt.txt
+wget https://api.wordpress.org/secret-key/1.1/salt/ -O /var/www/salt.txt
 sed -i '51,58d' /var/www/html/wp-config.php
 cat /var/www/html/salt.txt >> /var/www/html/wp-config.php
 cat /var/www/html/extra-wp-config.txt >> /var/www/html/wp-config.php
